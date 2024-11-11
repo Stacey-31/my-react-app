b2fd31a7c6eb98b8bd9d9e3713145e3bbc240be9
Stacey Acheampong <a.stacey@wustl.edu>
2024-11-11 00:33:11 -0600

    create const moves to check moves

--- src/App.jsx
+++ src/App.jsx
@@ -45,6 +45,7 @@ function Board({xIsNext, squares, onPlay}) {
     </div>
   </>);
 }
+
 export default function Game() {
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
@@ -54,13 +55,30 @@ export default function Game() {
     setHistory([...history, nextSquares]);
     setXIsNext(!xIsNext);
   }
+  function jumpTo(nextSquares) {
+    
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      <li>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
   return (
     <div className="game">
       <div className="game-board">
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
-        <ol>{/*TODO*/}</ol>
+        <ol>{moves}</ol>
       </div>
     </div>
   );
